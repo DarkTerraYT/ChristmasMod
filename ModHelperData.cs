98a9bc5d4a50b089d3c54efddb240d713f77a3e1
CubedLeaves <105320711+DarkTerraYT@users.noreply.github.com>
2024-02-07 22:03:00 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.1.0";
     public const string Name = "ChristmasMod";
 
-    public const string Description = "Adds a new tower. If you close out of the game you were playing in and you reload with middle path present launcher you'll need to re-buy it. Art + 3d models made by me and Popping Productions";
+    public const string Description = "Adds a new tower. If you close out of the game you were playing in and you reload with middle path present launcher you'll need to re-buy it. Art + 3d models made by me and Popping Productions. For questions or mod suggestions contact me on discord: @lerpmcgerk";
 
     public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "ChristmasMod"; // TODO add your repo name here, also in the download url in README.md
